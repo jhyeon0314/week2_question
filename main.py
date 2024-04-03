8c6941218466a47f4374f62bbfb46ed5cf77aa14
jhyeon0314 <52920187+jhyeon0314@users.noreply.github.com>
2024-04-04 00:51:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
   pq = PriorityQueue()
   pq.push("Task 1", 3)
   pq.push("Task 2", 1)
-  pq.push("Task 3", 2)  
+  pq.push("Task 3", 2)
   print(pq.pop())
   print(pq.pop())
   print(pq.pop())
